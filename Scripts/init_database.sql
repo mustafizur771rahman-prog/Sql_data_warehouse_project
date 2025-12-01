24c7c73b3d69c104a95cd790e81b459a73bc59e7
Mustafizur Rahman <mustafizur771rahman@gmail.com>
2025-12-02 00:52:02 +0600

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,14 @@
+-- Create Database 'DataWarehouse'
+
+USE master;
+
+CREATE DATABASE DataWarehouse;
+
+USE Datawarehouse;
+
+CREATE SCHEMA bronze;
+Go
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
